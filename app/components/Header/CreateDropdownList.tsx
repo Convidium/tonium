312f8c8484ac79e83ee9e3890a53f84b28c69b72
Convidium <denyssianchuk0@gmail.com>
2025-04-06 00:03:42 +0300

    Added basic structure of 'create' route layout

--- app/components/Header/CreateDropdownList.tsx
+++ app/components/Header/CreateDropdownList.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 import PlaySVG from "@/app/ui/icons/play.svg";
 import CreateSVG from "@/app/ui/icons/AddNew.svg";
 import ArtistSVG from "@/app/ui/icons/user.svg";
 import AlbumSVG from "@/app/ui/icons/record.svg";
 import SingleSVG from "@/app/ui/icons/single-icon.svg";
 import CompilationSVG from "@/app/ui/icons/collection-icon.svg";
-import SongSVG from "@/app/ui/icons/song.svg";
+import TrackSVG from "@/app/ui/icons/song.svg";
 import ArticleSVG from "@/app/ui/icons/article.svg";
 import "@/app/ui/styles/DropdownList.scss";
 
@@ -14,6 +15,11 @@ const CreateDropdownList: React.FC = () => {
   const blockRef = useRef<HTMLDivElement>(null);
   const listRef = useRef<HTMLDivElement>(null);
   const [isFocused, setIsFocused] = useState(false);
+
+  const router = useRouter();
+  const handleSelect = (type: string = "") => {
+    router.push(`/create/${type}`)
+  }
 
   const handleClickOutside = (event: MouseEvent) => {
     if (
@@ -36,34 +42,34 @@ const CreateDropdownList: React.FC = () => {
   return (
     <div className='header-element create-block' ref={blockRef} onClick={() => setIsFocused(!isFocused)}>
       <div className='skeumorphic-bg'>
-        <button className='create-btn'>
+        <button className='create-btn' onDoubleClick={() => handleSelect()}>
           <CreateSVG />
           <span>Create</span>
         </button>
       </div>
       <div className={`dropdown-list-wrapper ${isFocused ? "active" : "inactive"}`} ref={listRef}>
         <div className="dropdown-buttons">
-          <button className="dropdown-button">
+          <button className="dropdown-button" onClick={() => handleSelect("artist")}>
             <ArtistSVG />
             <span>Artist</span>
           </button>
-          <button className="dropdown-button">
+          <button className="dropdown-button" onClick={() => handleSelect("album")}>
             <AlbumSVG />
             <span>Album</span>
           </button>
-          <button className="dropdown-button">
+          <button className="dropdown-button" onClick={() => handleSelect("single")}>
             <SingleSVG />
             <span>Single</span>
           </button>
-          <button className="dropdown-button">
+          <button className="dropdown-button" onClick={() => handleSelect("compilation")}>
             <CompilationSVG />
             <span>Compilation</span>
           </button>
-          <button className="dropdown-button">
-            <SongSVG />
-            <span>Song</span>
+          <button className="dropdown-button" onClick={() => handleSelect("track")}>
+            <TrackSVG />
+            <span>Track</span>
           </button>
-          <button className="dropdown-button">
+          <button className="dropdown-button" onClick={() => handleSelect("article")}>
             <ArticleSVG />
             <span>Article</span>
           </button>
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+'use client'
+import { useRouter } from 'next/navigation';
+import React, { Component } from 'react';
+import "@/app/ui/styles/forms/formMain.scss";
+import "@/app/ui/styles/forms/formModels.scss";
+import AlbumSVG from "@/app/ui/icons/record.svg";
+import SingleSVG from "@/app/ui/icons/single-icon.svg";
+import ArtistSVG from "@/app/ui/icons/user.svg";
+import CompilationSVG from "@/app/ui/icons/collection-icon.svg";
+import ArticleSVG from "@/app/ui/icons/article.svg";
+import TrackSVG from "@/app/ui/icons/song.svg";
+import AddSVG from "@/app/ui/icons/fat-plus.svg";
+
+const options = [
+  { label: 'Album', icon: AlbumSVG, option1: "One artist", option2: "Multiple songs", option3: "One cover", value: 'album' },
+  { label: 'Single', icon: SingleSVG, option1: "One artist", option2: "One song", option3: "One cover", value: 'single' },
+  { label: 'Compilation', icon: CompilationSVG, option1: "Multiple artist", option2: "Multiple songs", option3: "Multiple covers", value: 'compilation' },
+  { label: 'Artist', icon: ArtistSVG, option1: "Basic artist info", option2: "Add artist albums", option3: "Add artist details", value: 'artist' },
+  { label: 'Article', icon: ArticleSVG, option1: "Create new article", option2: "Add article info", option3: "Quote other models", value: 'article' },
+  { label: 'Track', icon: TrackSVG, option1: "Basic track info", option2: "Add track lyrics", option3: "Add track details", value: 'track' },
+]
 
 function CreatePage() {
+  const router = useRouter();
+
+  const handleSelect = (type: string) => {
+    router.push(`/create/${type}`)
+  }
+
   return (
-    <div>CreatePage</div>
+    <div className='form-component form-desktop'>
+      <div className='form-wrapper'>
+        <div className='form-title'>Create new model</div>
+        <hr className='splitting-line' />
+        <div className='form-block'>
+          {options.map(({label, value, option1, option2, option3, icon: Component }, index) => (
+            <div className='form-create-option' key={index}>
+              <div className='option-title'>
+                {label}
+              </div>
+              <hr className='splitting-line' />
+              <div className='option-description'>
+                <div className='option-icon'>
+                  {<Component/>}
+                  </div>
+                <div className='option-text'>
+                  <span>{option1}</span>
+                  <span>{option2}</span>
+                  <span>{option3}</span>
+                </div>
+              </div>
+              <button className='option-btn' onClick={() => handleSelect(value)}>
+                <AddSVG/>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
